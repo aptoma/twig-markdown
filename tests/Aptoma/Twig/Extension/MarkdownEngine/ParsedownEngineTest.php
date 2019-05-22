625ce87d666740a8be7f9b8f0f3503b75e2c089b
Matthew Peveler <matt.peveler@gmail.com>
2019-05-22 10:52:32 -0400

    Add tests for new functions

--- tests/Aptoma/Twig/Extension/MarkdownEngine/ParsedownEngineTest.php
+++ tests/Aptoma/Twig/Extension/MarkdownEngine/ParsedownEngineTest.php
@@ -2,6 +2,7 @@
 
 namespace Aptoma\Twig\Extension\MarkdownEngine;
 
+use Aptoma\Twig\Extension\MarkdownExtension;
 use Aptoma\Twig\Extension\MarkdownExtensionTest;
 
 // Require parent class if not autoloaded
@@ -28,5 +29,33 @@ class ParsedownEngineTest extends MarkdownExtensionTest
     protected function getEngine()
     {
         return new ParsedownEngine();
+    }
+
+    public function testSafeMode()
+    {
+        $engine = $this->getEngine();
+        $loader = new \Twig\Loader\ArrayLoader(array('index' => '{{ "_Test_<em>Test</em>[xss](javascript:alert%281%29)"|markdown }}'));
+        $twig = new \Twig\Environment($loader, array('debug' => true, 'cache' => false));
+        $twig->addExtension(new MarkdownExtension($engine));
+
+        $this->assertEquals('<p><em>Test</em><em>Test</em><a href="javascript:alert%281%29">xss</a></p>', $twig->load('index')->render());
+
+        $engine->setSafeMode(true);
+        $this->assertEquals('<p><em>Test</em>&lt;em&gt;Test&lt;/em&gt;<a href="javascript%3Aalert%281%29">xss</a></p>', $twig->load('index')->render());
+        $engine->setSafeMode(false);
+    }
+
+    public function testMarkupEscape()
+    {
+        $engine = $this->getEngine();
+        $loader = new \Twig\Loader\ArrayLoader(array('index' => '{{ "_Test_<em>Test</em>[xss](javascript:alert%281%29)"|markdown }}'));
+        $twig = new \Twig\Environment($loader, array('debug' => true, 'cache' => false));
+        $twig->addExtension(new MarkdownExtension($engine));
+
+        $this->assertEquals('<p><em>Test</em><em>Test</em><a href="javascript:alert%281%29">xss</a></p>', $twig->load('index')->render());
+
+        $engine->setMarkupEscaped(true);
+        $this->assertEquals('<p><em>Test</em>&lt;em&gt;Test&lt;/em&gt;<a href="javascript:alert%281%29">xss</a></p>', $twig->load('index')->render());        
+        $engine->setMarkupEscaped(false);
     }
 }
